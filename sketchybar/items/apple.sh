7908085d235a339715c5023b6be510a24020b37e
AndreRD1026 <davilacesar125@gmail.com>
2024-01-12 17:07:22 -0600

    Sketchybar Files

--- sketchybar/items/apple.sh
+++ sketchybar/items/apple.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-sketchybar    --add item apple.logo left                            \
-            --set apple.logo icon=􀣺                                 \
-                            icon.color=$ACCENT_COLOR                \
-                            icon.padding_left=4                     \
-                            label.drawing=off                       \
-                            background.drawing=off                  \
-                            click_script="$PLUGIN_DIR/apple.sh"
+POPUP_OFF="sketchybar --set apple.logo popup.drawing=off"
+
+sketchybar --add item           apple.logo left                             \
+           --set apple.logo     icon=􀣺                                 \
+                                icon.color=$ACCENT_COLOR                           \
+                                label.drawing=off                           \
+                                background.drawing=off                      \
+                                click_script="$POPUP_CLICK_SCRIPT"          \
+                                                                            \
+           --add item           apple.prefs popup.apple.logo                \
+           --set apple.prefs    icon=􀺽                           \
+                                label="Preferences"                         \
+                                click_script="open -a 'System Preferences';
+                                              $POPUP_OFF"                   \
+           --add item           apple.activity popup.apple.logo             \
+           --set apple.activity icon=􀒓                              \
+                                label="Activity"                            \
+                                click_script="open -a 'Activity Monitor';
+                                              $POPUP_OFF"\
+           --add item           apple.lock popup.apple.logo                 \
+           --set apple.lock     icon=􀒳                                  \
+                                label="Lock Screen"                         \
+                                click_script="pmset displaysleepnow;
+                                              $POPUP_OFF"
